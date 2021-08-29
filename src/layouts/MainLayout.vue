d756ec665057011bb04d6605ad9a3630af1aef98
anndy2231 <5jose7@naver.com>
2021-08-29 21:34:02 +0900

    update

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,138 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar style="background: darkslateblue">
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+        <q-toolbar-title> Fancy App </q-toolbar-title>
+        <q-toolbar-title style="padding-right: 210px">
+          {{ currentYear }}년 {{ currentMonth }}월 {{ currentDate }}일
+          {{ currentHours }}:{{ currentMinutes }}:{{ currentSeconds }}
+          <q-spinner-hourglass color="white"></q-spinner-hourglass
+        ></q-toolbar-title>
+
+        <div>님 반갑습니다</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered class="bg-grey-1">
+      <q-list>
+        <q-item-label header class="text-grey-8">
+          Essential Links
+        </q-item-label>
+
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import EssentialLink from "components/EssentialLink.vue";
+
+const linksList = [
+  {
+    title: "포트폴리오",
+    caption: "https://portfolio-knduck.netlify.app/#/",
+    icon: "school",
+    link: "https://portfolio-knduck.netlify.app/#/",
+  },
+  {
+    title: "깃허브",
+    caption: "github.com/anndy2231",
+    icon: "code",
+    link: "https://github.com/anndy2231",
+  },
+  {
+    title: "Discord Chat Channel",
+    caption: "chat.quasar.dev",
+    icon: "chat",
+    link: "https://chat.quasar.dev",
+  },
+  {
+    title: "Forum",
+    caption: "forum.quasar.dev",
+    icon: "record_voice_over",
+    link: "https://forum.quasar.dev",
+  },
+  {
+    title: "Twitter",
+    caption: "@quasarframework",
+    icon: "rss_feed",
+    link: "https://twitter.quasar.dev",
+  },
+  {
+    title: "Facebook",
+    caption: "@QuasarFramework",
+    icon: "public",
+    link: "https://facebook.quasar.dev",
+  },
+  {
+    title: "Quasar Awesome",
+    caption: "Community Quasar projects",
+    icon: "favorite",
+    link: "https://awesome.quasar.dev",
+  },
+];
+
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "MainLayout",
+
+  components: {
+    EssentialLink,
+  },
+
+  setup() {
+    const leftDrawerOpen = ref(false);
+    var currentYear = ref();
+    var currentMonth = ref();
+    var currentDate = ref();
+    var currentHours = ref();
+    var currentMinutes = ref();
+    var currentSeconds = ref();
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+      currentYear,
+      currentMonth,
+      currentDate,
+      currentHours,
+      currentMinutes,
+      currentSeconds,
+      getTime() {
+        setInterval(() => {
+          var today = new Date();
+          currentYear.value = ref(today.getFullYear());
+          currentMonth.value = ref(today.getMonth() + 1);
+          currentDate.value = ref(today.getDate());
+          currentHours.value = ref(today.getHours());
+          currentMinutes.value = ref(today.getMinutes());
+          currentSeconds.value = ref(today.getSeconds());
+        }, 1000);
+      },
+    };
+  },
+  mounted() {
+    this.getTime();
+  },
+});
+</script>
